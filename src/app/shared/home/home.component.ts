34fae84256de2fabec97bc30f1f0d03c0151a5cf
MANINDER SINGH <maninder.singh@nagarro.com>
2021-04-21 18:39:37 +0530

    [Crowsel]:Crowsel added with random images

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,7 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
   ngOnInit(): void {
   }
 

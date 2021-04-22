de6cbb365c31271dfbb7ee1f4eb2224ac88dd465
MANINDER SINGH <maninder.singh@nagarro.com>
2021-04-22 22:05:18 +0530

    [Popup added]

--- src/app/shared/products/products/products.component.ts
+++ src/app/shared/products/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/core/models';
+import { NgbModal , ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -11,8 +12,10 @@ import { Product } from 'src/app/core/models';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor(private readonly route: ActivatedRoute) { }
+  constructor(private readonly route: ActivatedRoute,private modalService: NgbModal) { }
   product: Product;
+  addToCartProduct:Product;
+  closeResult = '';
   gridLayout=[1,2,3];
   num:any=[];
 grid:any=[];
@@ -35,6 +38,27 @@ grid:any=[];
 
   public _toggleSidebar() {
     this._opened = !this._opened;}
+    
+    open(content) {
+      this.modalService.open(content,
+     {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+        this.addToCartProduct=this.addToCartProduct;
+      }, (reason) => {
+        this.closeResult = 
+           `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    }
+    
+    private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return `with: ${reason}`;
+      }
+    }
 }
 
 

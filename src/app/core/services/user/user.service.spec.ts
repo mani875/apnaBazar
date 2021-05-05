1b14b2878f022b2e067b6ee0112b1ca0fbdb17ba
MANINDER SINGH <maninder.singh@nagarro.com>
2021-05-05 12:59:49 +0530

    user service unit tested

--- src/app/core/services/user/user.service.spec.ts
+++ src/app/core/services/user/user.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { HttpClient } from '@angular/common/http';
+import { LocalStorageService } from 'ngx-webstorage';
+import { of } from 'rxjs';
 
 describe('UserService', () => {
   let service: UserService;
+  let mockHttp: HttpClient;
+  let mockLocalStorageService: LocalStorageService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    // service = TestBed.inject(UserService);
+    service = new UserService(mockHttp, mockLocalStorageService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should return users data', () => {
+    let mockResponse = [
+      {
+        userName: 'Deepak',
+        password: 'Nagp@123',
+        level: 'silver',
+        name: 'Deepak Bisht',
+      },
+    ];
+    let response;
+    spyOn(service, 'getUsers').and.returnValue(of(mockResponse));
+    service.getUsers().subscribe((res) => {
+      response = res;
+    });
+    expect(response).toEqual(mockResponse);
   });
 });
